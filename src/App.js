d4cd024bfb65c0591994e5722ed207c9ccc848d4
tyler <tyler@cruftbusters.com>
2021-09-16 00:11:03 -0500

    ping pong with backend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { useState } from 'react'
+import useWebSocket from 'react-use-websocket'
+
 function App() {
-  return 'Hello, world!'
+  const [message, setMessage] = useState()
+  const { sendMessage, lastMessage } = useWebSocket(
+    'ws://6.tcp.ngrok.io:17616/ping',
+  )
+  return (
+    <div>
+      <input
+        onChange={(e) => setMessage(e.target.value)}
+        value={message}
+      />
+      <button
+        onClick={() => {
+          sendMessage(message)
+          setMessage('')
+        }}
+      >
+        send
+      </button>
+      <div>
+        {lastMessage ? lastMessage.data : 'Waiting for message...'}
+      </div>
+    </div>
+  )
 }
 
 export default App
