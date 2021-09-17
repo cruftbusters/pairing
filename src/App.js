6532a9921aff509f141b1ebdf7aa3de92c059a06
tyler <tyler@cruftbusters.com>
2021-09-17 01:01:28 -0500

    chatroom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import useWebSocket from 'react-use-websocket'
 
 function App() {
-  const [message, setMessage] = useState()
+  const [message, setMessage] = useState('')
+  const [history, setHistory] = useState([])
   const { sendMessage, lastMessage } = useWebSocket(
-    'wss://pairing-ws.cruftbusters.com/ping',
+    'wss://pairing-ws.cruftbusters.com/chat',
   )
+  useEffect(() => {
+    if (!lastMessage) return
+    setHistory((history) => history.concat([lastMessage.data]))
+  }, [lastMessage])
+
   return (
     <div>
+      {history.map((message, index) => (
+        <div key={index}>{message}</div>
+      ))}
       <input
         onChange={(e) => setMessage(e.target.value)}
         value={message}
@@ -15,14 +24,12 @@ function App() {
       <button
         onClick={() => {
           sendMessage(message)
+          setHistory((history) => history.concat([message]))
           setMessage('')
         }}
       >
         send
       </button>
-      <div>
-        {lastMessage ? lastMessage.data : 'Waiting for message...'}
-      </div>
     </div>
   )
 }
